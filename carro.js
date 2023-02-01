772d1af193103bd7344abcd84708bb707685f2b4
hoocarla <105882147+hoocarla@users.noreply.github.com>
2023-02-01 15:30:06 -0300

    Add files via upload

---
+++ carro.js
@@ -0,0 +1,31 @@
+//codigo do carro
+let xCarros =[600,600,600,600,600,600];
+let yCarros = [40, 96, 150,210,270];
+let velocidadeCarros=[2,2.5,3.2,5,2];
+let comprimentoCarro = 50;
+let alturaCarro = 40;
+// codigo do carro
+
+function mostraCarro() {
+  for (let i=0; i<imagemCarros.length; i=i+1){
+    image(imagemCarros[i], xCarros[i], yCarros[i],comprimentoCarro,alturaCarro);
+  }
+}
+
+function movimentaCarro(){
+  for (let i=0; i<velocidadeCarros.length; i=i+1){
+      xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+ for (let i = 0; i < imagemCarros.length; i = i + 1){
+  if (passouTodaTela(xCarros[i])){
+    xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaTela(xCarros){
+  return xCarros < -50;
+}
